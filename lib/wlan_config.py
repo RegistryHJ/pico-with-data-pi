34b25cf275082e6f36d45e80186738e9832f409f
RegistryHJ <registry.fordev@gmail.com>
2024-09-12 11:08:52 +0900

    add: WLANConfig

---
+++ lib/wlan_config.py
@@ -0,0 +1,18 @@
+class WLANConfig:
+  @staticmethod
+  def get_wlan_sen():
+    ssid = "senWiFi_Free"
+    password = "senhs2022@"
+    return ssid, password
+
+  @staticmethod
+  def get_wlan_ai_1():
+    ssid = "AI1"
+    password = "hsg12345"
+    return ssid, password
+
+  @staticmethod
+  def get_wlan_ai_2():
+    ssid = "AI2"
+    password = "hsg12345"
+    return ssid, password
